15702c5107118fdc1a8d94f7b34950899eea0a14
Kernel Mustang <Threat-Hunter@users.noreply.github.com>
2017-12-07 16:16:22 -0600

    Updated APA_8ball_Keeper
    Renamed collector.py to menu.py
    Moved all Player/Team creation to players.py (players class)
    Added comments to seasons.py and db.py

--- db.py
+++ db.py
@@ -3,15 +3,21 @@ import json
 
 class db:
     def json_check():
+## CHECK FOR TEAMS.JSON FILE, CREATE IT IF IT DOESN'T EXIST, AND OPEN THE JSON FILE
         if os.path.exists('teams.json') == False:
-            t = open('teams.json', 'w+')
+            teams = open('teams.json', 'w+') as teams_json
         else:
             with open('teams.json') as teams_json:
                 teams = json.load(teams_json)
+## CHECK FOR PLAYERS.JSON FILE, CREATE IT IF IT DOESN'T EXIST, AND OPEN THE JSON FILE
         if os.path.exists('players.json') == False:
-            p = open('players.json', 'w+')
+            players = open('players.json', 'w+') as players_json
         else:
             with open('players.json') as players_json:
                 players = json.load(players_json)
+## CHECK FOR SEASONS.JSON FILE, CREATE IT IF IT DOESN'T EXIST, AND OPEN THE JSON FILE
         if os.path.exists('season.json') == False:
-            s = open('season.json', 'w+')
+            season = open('season.json', 'w+') as season_json
+        else:
+            with open('seasons.json') as season_json:
+                season = json.load(season_json)
---
+++ menu.py
@@ -0,0 +1,79 @@
+import json
+import os
+from db import db
+from seasons import seasons
+from players import players
+
+def start():
+    print_header()
+    db.json_check()
+    seasons.archive_check()
+    menu()
+    menu_input()
+
+
+def print_header():
+    print("""
+    ___________________________________
+                                    ___`.
+   _______________________________,'   \ `.
+   ______________________________/,d$$$/   `.
+                ,-.     ,-.       `$$$`.     `.
+   :::::======o(   )   ((2))          `.`.     `.
+                `-'     `-'             `.`.     `.
+                         ,-.              `.`.     `.
+                        ((7))               `.`.     `.
+                         `-'                  `.`.
+     `Two-ball in the corner-pocket'            `.
+""")
+    print("--------------------------------------")
+    print("__APA 8 Ball Team & Player Collector__")
+    print("--------------------------------------")
+
+def menu():
+    print("------------------------------")
+    print("|       Menu Options         |")
+    print("------------------------------")
+    print("Start a [S]eason")
+    print("[A]rchive a Season")
+    print("Add a [T]eam")
+    print("Add a [P]layer (to a team)")
+    print("[R]emove a Player (from a team)")
+    print("[U]date a Player Skill Level")
+    print("[S]tart a Match!")
+    print("E[X]it")
+
+
+def menu_input():
+    menu_choice = ''
+    while menu_choice.lower() != 'x':
+        menu_choice = input("Selection: ").lower()
+        if menu_choice == 's':
+            seasons.season_start()
+            menu()
+        elif menu_choice == 'a':
+            seasons.archive_season()
+            menu()
+        elif menu_choice == 't':
+            plaerys.add_team()
+            menu()
+        elif menu_choice == 'p':
+            players.add_player()
+            menu()
+        elif menu_choice == 'r':
+            players.remove_player()
+            menu()
+        elif menu_choice == 'u':
+            players.update_player()
+            menu()
+        elif menu_choice == 's':
+            print("Let's play some pool!")
+        elif menu_choice == 'x':
+            print("Exiting...")
+            exit()
+        else:
+            print("No valid selection given, please try again.")
+
+players = {}
+teams = {}
+start()
